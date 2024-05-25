ddf7971e2559c205555bc6c056faf182454e51ac
Fadil <fadil@suitmedia.com>
2024-05-25 09:07:46 +0700

    Initial Commit

---
+++ Fadil Card/Fadil_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fadil_CardApp.swift
+//  Fadil Card
+//
+//  Created by Fadil Kurniawan on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Fadil_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

8d8bada8328d4864a2155e02275f72feda97e22c
Fadil <fadil@suitmedia.com>
2024-05-25 17:43:34 +0700

    f

--- Fadil Card/ContentView.swift
+++ Fadil Card/ContentView.swift
@@ -9,16 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.init(hex: "#16a085")
+                .ignoresSafeArea()
+            VStack {
+                Image("fadil").resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150,height: 150)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                
+                Text("Fadil K")
+                    .font(Font.custom("DancingScript-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("Mobile Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                
+                ContactView(text: "+62 821 98592939", imageName: "phone.fill")
+                ContactView(text: "fadil30.fk@gmail.com", imageName: "envelope.fill")
+            }
+            
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct ContactView: View {
+    let text : String
+    let imageName : String
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .frame(height: 50)
+            .foregroundColor(.white)
+            .overlay(
+                HStack {
+                    Image(systemName: imageName)
+                        .foregroundColor(.green)
+                    Text(text)
+                        .font(.system(size: 18))
+                        .foregroundColor(.black)
+                        .bold()
+                }
+            ).padding(.all)
+    }
 }
